7042eba27f980ff9981ccd6da14d9f4222e9de90
feles-ao42 <ao.42ao.42@gmail.com>
2023-03-10 18:28:12 +0900

    fix zsh

--- etc/init/dein-vim-install.sh
+++ etc/init/dein-vim-install.sh
@@ -1,2 +1,2 @@
 mkdir ~/.cache/dein -p
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s -- ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | zsh -s -- ~/.cache/dein
